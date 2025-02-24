d21eb7864639782d6d7f18189b2db318b365226b
Natasha Nduku <natashanduku0@gmail.com>
2025-02-24 12:57:11 +0300

    anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+# lib/anagram.py
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower() 
+        self.sorted_word = sorted(self.word)  
+    def match(self, word_list):
+        
+        return [word for word in word_list if sorted(word.lower()) == self.sorted_word and word.lower() != self.word]
+
+# Example usage:
+listen = Anagram("listen")
+print(listen.match(['enlists', 'google', 'inlets', 'banana']))  # Output: ['inlets']
